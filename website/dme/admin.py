164953bbb01ae560acf057fa6f5696137d27b904
Derek Hon <derek.hon@outlook.com>
2023-03-03 14:17:45 -0500

    Deleting website project from here
    Going to create separate firebase/django website. Statis website here willl also exist

--- website/dme/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import DMEConfig, DME_Map, fitness
-# Register your models here.
-
-admin.site.register(DMEConfig)
-admin.site.register(DME_Map)
-admin.site.register(fitness)
-
--- website/dme/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import DMEConfig
-
-# class DMEConfigForm(forms.ModelForm):
-#     class Meta:
-#         model= DMEConfig
-#         fields= ["name", "imagefile"]
